d61d21e4f7bd01da9b2a5ea6a72983298e39fd5b
Admin <admin@Admins-MacBook-Pro.local>
2024-07-18 17:45:14 +0530

    third commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("welcome");
+		System.out.println("welcome to spring boot");
 	}
 
 }
