863a3e5c568d9b7828e87c676b58f7c2bbe59561
Admin <admin@Admins-MacBook-Pro.local>
2024-07-19 21:50:27 +0530

    crud repo and MVC design created

--- src/main/java/com/example/model/CashCard.java
+++ src/main/java/com/example/model/CashCard.java
@@ -1,9 +1,13 @@
 package com.example.model;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 public class CashCard {
 
     private long id;
     private double amount;
+   
+
 
     public CashCard(long id,double amount){
         this.id = id;
